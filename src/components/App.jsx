2fdf5500a4ae0423e9ad0bb374afb3716cf26800
AleksanderSuchodolski <129153686+AleksanderSuchodolski@users.noreply.github.com>
2023-11-05 01:26:40 +0100

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import user from "../datajson/user.json";
+import { Profile } from "./Profile/Profile";
+
+import styles from "./App.module.css"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.container}>
+      <h1>Profile</h1>
+     <Profile
+     username={user.username}
+     tag={user.tag}
+     location={user.location}
+     avatar={user.avatar}
+     stats={user.stats}
+    />
+      
     </div>
   );
 };
